80e1a0f4fe5ff78477b8c8d53372e133bc788b0b
Deri Kurniawan <deri.netuchi@gmail.com>
2021-10-30 19:55:17 +0700

    import json data

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -2,9 +2,11 @@ import '../components/app-bar';
 import '../components/hero-image';
 import '../components/restaurant-list';
 import '../components/restaurant-item';
+import dataJSON from '../../DATA.json';
+import $ from 'jquery';
 
 const main = () => {
-    
+    const restaurants = JSON.parse(JSON.stringify(dataJSON)).restaurants;
 }
 
 export default main;
