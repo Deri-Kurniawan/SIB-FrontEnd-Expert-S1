c1b57d7e8fee1a384ae935a6e8acc3c63926cf68
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-03 08:57:03 +0700

    adding toggler nav scripts

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -8,8 +8,8 @@ const main = () => {
     restaurants.forEach((restaurant) => {
 
         restaurantElement.innerHTML += `
-        <div class="restaurant-item card">
-            <img class="card-image" src="${restaurant.pictureId}"/>
+        <div class="restaurant-item card" data-id="${restaurant.id}" tabindex="0">
+            <img class="card-image" src="${restaurant.pictureId}" alt="${restaurant.name} Restaurant Image"/>
             <div class="card-body">
                 <span>Rating <i class="stars fas fa-star fa-fw"></i> ${restaurant.rating}</span>
                 <h2 class="card-title">${restaurant.name}</h2>
@@ -17,7 +17,21 @@ const main = () => {
             </div>
         </div>
         `;
+    });
 
+    const menuToggleElement = document.querySelector('#menu-toggle');
+
+    menuToggleElement.addEventListener('click', () => {
+        const menuToggleIconElement = document.querySelector('#menu-toggle .icon');
+        document.querySelector('.nav-menu').classList.toggle('menu-open');
+
+        const replaced = menuToggleIconElement.classList.replace('fa-bars', 'fa-times');
+
+        if(!replaced) {
+            menuToggleIconElement.classList.replace('fa-times', 'fa-bars');
+        }
+
+        
     });
 }
 
