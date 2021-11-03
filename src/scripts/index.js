e0d87c2d057405defb45bd1b7cd15d7d3caba5dc
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-03 12:36:21 +0700

    apply code style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,6 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '@fortawesome/fontawesome-free/js/all';
 import '../styles/main.css';
 import '../styles/responsive.css';
-import main from '../scripts/view/main';
+import main from './view/main';
 
 document.addEventListener('DOMContentLoaded', main);
